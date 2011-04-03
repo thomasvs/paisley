60ab58d8573b150882f9bdc003d2971ec7f922f9
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2011-04-04 00:10:18 +0200

    Add a caching implementation.
    Conflicts:
    
    	paisley/client.py

---
+++ paisley/test_cache.py
@@ -0,0 +1,69 @@
+# -*- test-case-name: paisley.test_cache -*-
+
+# Copyright (c) 2007-2008
+# See LICENSE for details.
+
+"""
+Test for couchdb client caching implementation.
+"""
+
+from twisted.internet import defer
+
+from paisley import client
+
+from paisley.test import util
+
+
+class MemoryCacheTestCase(util.CouchDBTestCase):
+
+    def setUp(self):
+        util.CouchDBTestCase.setUp(self)
+
+        self.cache = client.MemoryCache()
+        self.db = client.CouchDB('localhost', port=self.db.port,
+            username='testpaisley', password='testpaisley',
+            cache=self.cache)
+
+        d = defer.Deferred()
+        d.addCallback(lambda _: self.db.createDB('test'))
+        d.addCallback(lambda _: self.db.saveDoc('test', {
+            'key': 'value'
+        }))
+        d.addCallback(lambda r: setattr(self, 'first', r['id']))
+        d.addCallback(lambda _: self.db.saveDoc('test', {
+            'lock': 'chain'
+        }))
+        d.addCallback(lambda r: setattr(self, 'second', r['id']))
+        d.callback(None)
+        return d
+
+    def testCached(self):
+        
+        d = defer.Deferred()
+
+        d.addCallback(lambda _: self.db.openDoc('test', self.first))
+        def openCb(result):
+            self.assertEquals(result['key'], 'value')
+        d.addCallback(openCb)
+        d.addCallback(lambda _: self.assertEquals(self.cache.lookups, 1))
+        d.addCallback(lambda _: self.assertEquals(self.cache.hits, 0))
+        d.addCallback(lambda _: self.assertEquals(self.cache.cached, 1))
+
+        d.addCallback(lambda _: self.db.openDoc('test', self.first))
+        def openCb(result):
+            self.assertEquals(result['key'], 'value')
+        d.addCallback(openCb)
+        d.addCallback(lambda _: self.assertEquals(self.cache.lookups, 2))
+        d.addCallback(lambda _: self.assertEquals(self.cache.hits, 1))
+        d.addCallback(lambda _: self.assertEquals(self.cache.cached, 1))
+
+        d.addCallback(lambda _: self.db.openDoc('test', self.second))
+        def openCb(result):
+            self.assertEquals(result['lock'], 'chain')
+        d.addCallback(openCb)
+        d.addCallback(lambda _: self.assertEquals(self.cache.lookups, 3))
+        d.addCallback(lambda _: self.assertEquals(self.cache.hits, 1))
+        d.addCallback(lambda _: self.assertEquals(self.cache.cached, 2))
+
+        d.callback(None)
+        return d
