bc56b58e2f14b368ea57010389279a976528631b
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2015-03-03 22:39:27 -0500

    id's suffered from the same problem; thanks hoodie

--- paisley/client.py
+++ paisley/client.py
@@ -365,7 +365,8 @@ class CouchDB(object):
             assert type(revision) is unicode, \
                 'revision is %r instead of unicode' % (type(revision), )
 
-        uri = "/%s/%s" % (_namequote(dbName), quote(docId.encode('utf-8')))
+        uri = "/%s/%s" % (_namequote(dbName),
+            _namequote(docId).encode('utf-8'))
         if revision is not None:
             uri += "?%s" % (urlencode({"rev": revision.encode('utf-8')}), )
         elif full:
@@ -419,7 +420,7 @@ class CouchDB(object):
             body = json.dumps(body)
         if docId is not None:
             d = self.put("/%s/%s" % (_namequote(dbName),
-                quote(docId.encode('utf-8'))),
+                _namequote(docId).encode('utf-8')),
                 body, descr='saveDoc')
         else:
             d = self.post("/%s/" % (_namequote(dbName), ), body,
@@ -454,7 +455,7 @@ class CouchDB(object):
 
         return self.delete("/%s/%s?%s" % (
                 _namequote(dbName),
-                quote(docId.encode('utf-8')),
+                _namequote(docId).encode('utf-8'),
                 urlencode({'rev': revision.encode('utf-8')}))).addCallback(
                     self.parseResult)
 
@@ -469,7 +470,8 @@ class CouchDB(object):
 
         def buildUri(dbName=dbName, docId=docId, viewId=viewId, kwargs=kwargs):
             return "/%s/_design/%s/_view/%s?%s" % (
-                _namequote(dbName), quote(docId), viewId, urlencode(kwargs))
+                _namequote(dbName), _namequote(docId).encode('utf-8'),
+                viewId, urlencode(kwargs))
 
         # if there is a "keys" argument, remove it from the kwargs
         # dictionary now so that it doesn't get double JSON-encoded
